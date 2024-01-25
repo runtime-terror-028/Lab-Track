75f3c10917b0e3eba9d203303bb8246719464452
runtime-terror-028 <runtime.terror.028@gmail.com>
2024-01-25 15:30:39 +0530

    first commit

---
+++ server.py
@@ -0,0 +1,14 @@
+import socket
+s = socket.socket()
+print('socket succesfully created')
+port = 55555
+s.bind(('', port))
+print(f'socket binded to port {port}')
+s.listen(5) #<----clent limit
+print('socket is listening')
+while True:
+    c, addr = s.accept()
+    print('got connection from', addr)
+    message = ('Thank you for connecting')
+    c.send(message.encode())
+    c.close()

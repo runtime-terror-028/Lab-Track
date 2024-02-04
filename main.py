44746e16b2130c1625845930c233f4a71fe2e820
runtime-terror-028 <runtime.terror.028@gmail.com>
2024-02-04 20:26:49 +0530

    Login window GUI working

--- main.py
+++ main.py
@@ -1,70 +1,65 @@
-import server
-import client
 import tkinter as tk
-from tkinter import messagebox
 
-class login():
-    def login_admin():
-        window_admin = tk.Tk()
-        window_admin.title("Admin")
+class login:
+    def __init__(self):
+        self.window_ask = tk.Tk()
+        self.window_ask.title("ask")
 
-        username_label = tk.Label(text="Username:")
-        username_label.grid(row=0, column=0, sticky="e", pady=5)
-        username_entry = tk.Entry()
-        username_entry.grid(row=0, column=1, pady=5)
-
-        password_label = tk.Label(text="Password:")
-        password_label.grid(row=1, column=0, sticky="e", pady=5)
-        password_entry = tk.Entry(show="*")
-        password_entry.grid(row=1, column=1, pady=5)
-
-        login_button = tk.Button(text="Login")
-        login_button.grid(row=2, column=0, columnspan=2, pady=10)
-
-        window_admin.mainloop()
-    
-    def login_client():
-        window_admin = tk.Tk()
-        window_admin.title("Admin")
-        
-        username_label = tk.Label(text="Username:")
-        username_label.grid(row=0, column=0, sticky="e", pady=5)
-        username_entry = tk.Entry()
-        username_entry.grid(row=0, column=1, pady=5)
-
-        password_label = tk.Label(text="Password:")
-        password_label.grid(row=1, column=0, sticky="e", pady=5)
-        password_entry = tk.Entry(show="*")
-        password_entry.grid(row=1, column=1, pady=5)
-
-        login_button = tk.Button(text="Login")
-        login_button.grid(row=2, column=0, columnspan=2, pady=10)
-
-        window_admin.mainloop()
-    
-    def login_ask():
-        window_ask = tk.Tk()
-        window_ask.title("ask")
-
-        label = tk.Label(text="Choose User Type:")
+        label = tk.Label(self.window_ask, text="Choose User Type:")
         label.pack(pady=10)
 
-        option1_button = tk.Button(text="Student")
+        option1_button = tk.Button(self.window_ask, text="Student", command=self.login_kill_client)
         option1_button.pack(pady=5)
 
-        option2_button = tk.Button(text="Admin", command = login.login_admin())
+        option2_button = tk.Button(self.window_ask, text="Admin", command=self.login_kill_admin)
         option2_button.pack(pady=5)
 
-        window_ask.mainloop()
+        self.window_ask.mainloop()
 
-user_session= False
-user_type = "Admin"
+    def login_admin(self):
+        window_admin = tk.Tk()
+        window_admin.title("Admin")
 
-if user_session == True and user_type == "Admin":
-    login.login_admin()
-elif user_session == True and user_type == "Client":
-    login.login_client()
-elif user_session == False:
-    login.login_ask()
-else:
-    print("session error")
+        username_label = tk.Label(text="Username:")
+        username_label.grid(row=0, column=0, sticky="e", pady=5)
+        username_entry = tk.Entry()
+        username_entry.grid(row=0, column=1, pady=5)
+
+        password_label = tk.Label(text="Password:")
+        password_label.grid(row=1, column=0, sticky="e", pady=5)
+        password_entry = tk.Entry(show="*")
+        password_entry.grid(row=1, column=1, pady=5)
+
+        login_button = tk.Button(text="Login")
+        login_button.grid(row=2, column=0, columnspan=2, pady=10)
+
+        window_admin.mainloop()
+    
+    def login_client(self):
+        window_client = tk.Tk()
+        window_client.title("Client")
+        
+        username_label = tk.Label(window_client, text="Username:")
+        username_label.grid(row=0, column=0, sticky="e", pady=5)
+        username_entry = tk.Entry(window_client)
+        username_entry.grid(row=0, column=1, pady=5)
+
+        password_label = tk.Label(window_client, text="Password:")
+        password_label.grid(row=1, column=0, sticky="e", pady=5)
+        password_entry = tk.Entry(window_client, show="*")
+        password_entry.grid(row=1, column=1, pady=5)
+
+        login_button = tk.Button(window_client, text="Login")
+        login_button.grid(row=2, column=0, columnspan=2, pady=10)
+
+        window_client.mainloop()
+    
+    def login_kill_admin(self):
+        self.window_ask.destroy()
+        self.login_admin()
+
+    def login_kill_client(self):
+        self.window_ask.destroy()
+        self.login_client()
+    
+login_instance = login()

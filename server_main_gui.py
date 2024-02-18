739be4ae876459e10ffad475b22e13b451c801ba
runtime-terror-028 <runtime.terror.028@gmail.com>
2024-02-18 16:19:19 +0530

    fixed textbox

--- server_main_gui.py
+++ server_main_gui.py
@@ -28,19 +28,19 @@ class Admin_Main():#<---------main admin window
         image_image_1 = PhotoImage(
             file=("assets/admin_bg.png"))
         image_1 = canvas.create_image(
-                424.0,
-                305.0,
-                image=image_image_1
-            )
+            424.0,
+            305.0,
+            image=image_image_1
+        )
 
-        canvas.place(x = 0, y = 0)
         image_image_2 = PhotoImage(
             file=("assets/kiit.png"))
-        image_1 = canvas.create_image(
+        image_2 = canvas.create_image(
             424.0,
             44.0,
             image=image_image_2
         )
+
         canvas.create_text(
             366.0,
             92.0,
@@ -50,21 +50,45 @@ class Admin_Main():#<---------main admin window
             font=("Inter Bold", 24 * -1)
         )
 
-        canvas.create_rectangle(
-            336.0,
-            145.0,
-            830.0,
-            590.0,
-            fill="#D9D9D9",
-            outline="")
+        # entry_image_1 = PhotoImage(
+        #     file=("assets/aentry_1.png"))
+        # entry_bg_1 = canvas.create_image(
+        #     583.0,
+        #     367.5,
+        #     image=entry_image_1
+        # )
+        entry_1 = Text(
+            bd=0,
+            bg="#D9D9D9",
+            fg="#000716",
+            highlightthickness=0
+        )
+        entry_1.place(
+            x=336.0,
+            y=145.0,
+            width=494.0,
+            height=443.0
+        )
 
-        canvas.create_rectangle(
-            21.0,
-            266.0,
-            307.0,
-            590.0,
-            fill="#D9D9D9",
-            outline="")
+        # entry_image_2 = PhotoImage(
+        #     file=("assets/aentry_1.png"))
+        # entry_bg_2 = canvas.create_image(
+        #     164.0,
+        #     428.0,
+        #     image=entry_image_2
+        # )
+        entry_2 = Text(
+            bd=0,
+            bg="#D9D9D9",
+            fg="#000716",
+            highlightthickness=0
+        )
+        entry_2.place(
+            x=21.0,
+            y=266.0,
+            width=286.0,
+            height=322.0
+        )
 
         canvas.create_rectangle(
             21.0,
@@ -73,7 +97,7 @@ class Admin_Main():#<---------main admin window
             238.0,
             fill="#D9D9D9",
             outline="")
-        
+
         canvas.create_text(
             34.0,
             155.0,
@@ -83,13 +107,25 @@ class Admin_Main():#<---------main admin window
             font=("Inter Bold", 13 * -1)
         )
 
-        canvas.create_rectangle(
-            134.0,
-            185.0,
-            288.0,
-            218.0,
-            fill="#FFFFFF",
-            outline="")
+        # entry_image_3 = PhotoImage(
+        #     file=("assets/aentry_3.png"))
+        # entry_bg_3 = canvas.create_image(
+        #     211.0,
+        #     201.5,
+        #     image=entry_image_3
+        # )
+        entry_3 = Entry(
+            bd=0,
+            bg="#FFFFFF",
+            fg="#000716",
+            highlightthickness=0
+        )
+        entry_3.place(
+            x=134.0,
+            y=185.0,
+            width=154.0,
+            height=31.0
+        )
 
         button_image_1 = PhotoImage(
             file=("assets/change_port_button.png"))
@@ -97,14 +133,14 @@ class Admin_Main():#<---------main admin window
             image=button_image_1,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: print("button_1 clicked"),
+            command=lambda: print("change port clicked"),
             relief="flat"
         )
         button_1.place(
             x=40.0,
             y=185.0,
             width=77.0,
-            height=30.0
+            height=33.0
         )
 
         self.window.resizable(False, False)
